1b91a12bb1b18f38274fd507663f71f87d63e7ac
gaojikai <gaojikai@fehorizon.com>
2024-03-28 16:40:15 +0800

    feat:文件操作相关api

--- node/stream/fsOperate.js
+++ node/stream/fsOperate.js
@@ -15,6 +15,7 @@
 
 // 删除某个文件夹
 
+const { dir } = require('console');
 const fs = require('fs');
 const path = require('path');
 ;
@@ -44,8 +45,122 @@ function rmdirSync(directory) {
         fs.unlinkSync(directory);
     } else {
         let dirs = fs.readdirSync(directory);
-        dirs = dirs.map(dir => path.resolve(directory, dir));
+        dirs = dirs.map(dir => readdirSync(path.join(directory, dir)));
         fs.rmdirSync(directory)
     }
 }
 rmdirSync('a')
+
+//异步删除 树 树的遍历方式有两种 (层序遍历 深度遍历(先序 中序 后序))
+
+// webpack 异步串行 异步并行 
+
+function rmdir(directory, cb) {
+    fs.stat(directory, (err, statObj) => {
+        if (statObj.isFile()) {
+            fs.unlink(directory, cb)
+        } else {
+            fs.readdir(directory, (err, dirs) => {
+                const dirs = dirs.map(dir => path.join(directory, dir));
+                let index = 0;
+                function next() {
+                    // 递归删除
+                    if (index === dirs.length) return fs.rmdir(directory, cb);
+                    dir = dirs[index++];
+                    // 删除完毕后继续删除下一个
+                    rmdir(dir, next);
+                }
+                next();
+
+            })
+        }
+
+
+    })
+
+
+}
+
+// 上述代码优化
+function rmdir(directory, cb) {
+    fs.stat(directory, (err, statObj) => {
+        if (statObj.isFile()) {
+            fs.unlink(directory, cb)
+        } else {
+            fs.readdir(directory, (err, dirs) => {
+                if (dir.length === 0) {
+                    return fs.rmdir(directory, cb);
+                }
+
+                const dirs = dirs.map(dir => path.join(directory, dir));
+
+                function done() {
+                    if (++index === dirs.length) {
+                        fs.rmdir(directory, cb)
+                    }
+                }
+
+                // 并发删除
+                dirs.forEach(dir => {
+                    rmdir(dir, () => {
+                        fs.rmdir(directory, done);
+                    })
+                })
+
+
+            })
+        }
+
+
+    })
+
+
+}
+
+
+// 将代码改成promise
+function rmdir(directory, cb) {
+    return new Promise((resolve, reject) => {
+        fs.stat(directory, (err, statObj) => {
+            if (statObj.isFile()) {
+                fs.unlink(directory, resolve)
+            } else {
+                fs.readdir(directory, (err, dirs) => {
+                    const dirs = dirs.map(dir => rmdir(path.join(directory, dir)));
+                    Promise.all(dirs).then(() => {
+                        fs.rmdir(directory, resolve)
+                    })
+
+                })
+            }
+        })
+    })
+
+}
+
+//async awit
+const fsPromises = require('fs/promises');
+async function rmdir(directory, cb) {
+    const statObj = await fsPromises.stat(directory)
+    if (statObj.isFile()) {
+        return fsPromises.unlink(directory)
+    } else {
+        let dirs = await fsPromises.readdir(directory)
+        dirs = dirs.map(dir => rmdir(path.join(directory, dir)));
+        await Promise.all(dirs)
+        return fs.rmdir(directory)
+
+    }
+
+}
+
+rmdir('a').then(() => {
+    console.log('删除成功');
+})
+
+
+rmdir('a', () => {
+    console.log('删除成功');
+})
+
+// 层序遍历来删除目录 
